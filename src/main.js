f8720114d4d0fe930ff49e9d6cbe61a5397c81a7
Katarzyna Kmita <kmita.katarzyna.a@gmail.com>
2023-03-08 13:50:57 +0100

    commenting out  filters

--- src/main.js
+++ src/main.js
@@ -46,7 +46,7 @@ const textureLoader = new THREE.TextureLoader();
 
 const renderer = new THREE.WebGLRenderer({
   canvas,
-  antialias: false,
+  // antialias: false,
   powerPreference: "high-performance",
   alpha: true
 });
@@ -115,73 +115,72 @@ iridescenceLookUp.boost = 5;
 // OUTLINE EFFECT
 //
 // Set up post processing
-const depthTexture = new THREE.DepthTexture();
-const outlineRenderTarget = new THREE.WebGLRenderTarget(
-  window.innerWidth,
-  window.innerHeight,
-  {
-    depthTexture: depthTexture,
-    depthBuffer: true,
-  }
-);
-// Initial render pass
-const outlineComposer = new EffectComposer(renderer, outlineRenderTarget);
-const pass = new RenderPass(outlineScene, outlineCamera);
-outlineComposer.addPass(pass);
-// Outline pass
-const customOutline = new CustomOutlinePass(
-  new THREE.Vector2(window.innerWidth, window.innerHeight),
-  outlineScene,
-  outlineCamera
-);
-outlineComposer.addPass(customOutline);
-// Antialias pass
-const effectFXAA = new ShaderPass(FXAAShader);
-effectFXAA.uniforms["resolution"].value.set(
-  1 / window.innerWidth,
-  1 / window.innerHeight
-  );
-outlineComposer.addPass(effectFXAA);
-// adjustments
-const uniforms = customOutline.fsQuad.material.uniforms;
-uniforms.outlineColor.value.set(0x999999);
+// const depthTexture = new THREE.DepthTexture();
+// const outlineRenderTarget = new THREE.WebGLRenderTarget(
+//   window.innerWidth,
+//   window.innerHeight,
+//   {
+//     depthTexture: depthTexture,
+//     depthBuffer: true,
+//   }
+// );
+// // Initial render pass
+// const outlineComposer = new EffectComposer(renderer, outlineRenderTarget);
+// const pass = new RenderPass(outlineScene, outlineCamera);
+// outlineComposer.addPass(pass);
+// // Outline pass
+// const customOutline = new CustomOutlinePass(
+//   new THREE.Vector2(window.innerWidth, window.innerHeight),
+//   outlineScene,
+//   outlineCamera
+// );
+// outlineComposer.addPass(customOutline);
+// // Antialias pass
+// const effectFXAA = new ShaderPass(FXAAShader);
+// effectFXAA.uniforms["resolution"].value.set(
+//   1 / window.innerWidth,
+//   1 / window.innerHeight
+//   );
+// outlineComposer.addPass(effectFXAA);
+// // adjustments
+// const uniforms = customOutline.fsQuad.material.uniforms;
+// uniforms.outlineColor.value.set(0x999999);
 
 
 //
 // FLOWER
 //
-let mixer;
-glbLoader.load('src/models/daisy_flower.glb', (gltf) => {
-  const flowerModel = gltf.scene;
-  flowerModel.name = 'flower';
-  flowerModel.position.set(-10, -32, 0);
-  flowerModel.scale.set(2, 2, 2);
-  flowerModel.rotation.set(0.3, 270, -0.2)
-  // outlineScene.add(flowerModel); 
+// glbLoader.load('src/models/daisy_flower.glb', (gltf) => {
+//   const flowerModel = gltf.scene;
+//   flowerModel.name = 'flower';
+//   flowerModel.position.set(-10, -32, 0);
+//   flowerModel.scale.set(2, 2, 2);
+//   flowerModel.rotation.set(0.3, 270, -0.2)
+//   outlineScene.add(flowerModel); 
 
-  // material adjustments
-  const flowerMaterial = flowerModel.getObjectByName('Object_9').material;
+//   // material adjustments
+//   const flowerMaterial = flowerModel.getObjectByName('Object_9').material;
 
-  flowerMaterial.color = { r: 1, g: 0, b: 1}
-  flowerMaterial.map = undefined;
+//   flowerMaterial.color = { r: 1, g: 0, b: 1}
+//   flowerMaterial.map = undefined;
 
-  flowerMaterial.emissive = {r: 0.80, g: 1, b: 0.85};
-  flowerMaterial.emissiveMap = textureLoader.load('src/img/texture4.jpg');
-  flowerMaterial.emissiveIntensity = 1;
+//   flowerMaterial.emissive = {r: 0.80, g: 1, b: 0.85};
+//   flowerMaterial.emissiveMap = textureLoader.load('src/img/texture4.jpg');
+//   flowerMaterial.emissiveIntensity = 1;
 
-  flowerMaterial.metalness = 1;
-  flowerMaterial.roughness  = 0.25;
-  flowerMaterial.reflectivity = 1;
-  flowerMaterial.transmission = 1;
-  flowerMaterial.iridescence = 1;
-  flowerMaterial.attenuationColor = {r: 1, g: 0, b: 1};
+//   flowerMaterial.metalness = 1;
+//   flowerMaterial.roughness  = 0.25;
+//   flowerMaterial.reflectivity = 1;
+//   flowerMaterial.transmission = 1;
+//   flowerMaterial.iridescence = 1;
+//   flowerMaterial.attenuationColor = {r: 1, g: 0, b: 1};
   
-  flowerMaterial.sheen = 1;
-  flowerMaterial.sheenRoughness = 1;
+//   flowerMaterial.sheen = 1;
+//   flowerMaterial.sheenRoughness = 1;
   
-}, undefined, function (error) {
-console.error(error);
-});
+// }, undefined, function (error) {
+// console.error(error);
+// });
 
 //
 // SPLASHED LIQUID MODEL
@@ -212,12 +211,16 @@ const objectsDistance = 50
 //
 glbLoader.load('src/models/smile_icon.glb', (gltf) => {
   const smileModel = gltf.scene;
-  smileModel.name = "smile";
   smileModel.position.set(20, -9, 44);
-  smileModel.rotateY(0.2)
-  // smileModel.position.set(2, -3, -10);
+  smileModel.rotateY(1.5)
   smileModel.scale.set(70, 70, 70);
   scene.add(smileModel); 
+
+  let smallSmile = smileModel.clone();
+  smallSmile.name = "smile";
+  smallSmile.position.set(2, -3, -10);
+  smallSmile.position.y -= objectsDistance;
+  scene.add(smallSmile)
 
   scene.traverse(function(object) {
     if (object.material) 
@@ -235,7 +238,7 @@ glbLoader.load('src/models/education.glb', (gltf) => {
   const educationModel = gltf.scene;
   educationModel.name = "education";
   educationModel.position.set(2.5, -6, -10);
-  educationModel.position.y -= objectsDistance;
+  educationModel.position.y -= objectsDistance * 2;
   educationModel.scale.set(8, 11, 8);
   educationModel.rotateX(0.3);
   scene.add(educationModel); 
@@ -256,7 +259,7 @@ glbLoader.load('src/models/laptop.glb', (gltf) => {
   const laptopModel = gltf.scene;
   laptopModel.name = "laptop";
   laptopModel.position.set(5, -9, -10);
-  laptopModel.position.y -= objectsDistance * 2;
+  laptopModel.position.y -= objectsDistance * 3;
   laptopModel.scale.set(0.17, 0.17, 0.17);
   laptopModel.rotateX(0.3);
   laptopModel.rotateY(0.2)
@@ -278,7 +281,7 @@ glbLoader.load('src/models/typing_bubble.glb', (gltf) => {
   const bubbleModel = gltf.scene;
   bubbleModel.name = "bubble";
   bubbleModel.position.set(3, 0, -10);
-  bubbleModel.position.y -= objectsDistance * 3;
+  bubbleModel.position.y -= objectsDistance * 4;
   bubbleModel.scale.set(6.5, 7, 6.5);
   bubbleModel.rotateX(1.5);
   scene.add(bubbleModel); 
@@ -299,21 +302,19 @@ function onWindowResize() {
   outlineCamera.updateProjectionMatrix();
 
   renderer.setSize(window.innerWidth, window.innerHeight);
-  outlineComposer.setSize(window.innerWidth, window.innerHeight);
-  effectFXAA.setSize(window.innerWidth, window.innerHeight);
-  customOutline.setSize(window.innerWidth, window.innerHeight);
-  effectFXAA.uniforms["resolution"].value.set(
-    1 / window.innerWidth,
-    1 / window.innerHeight
-  );
+  // outlineComposer.setSize(window.innerWidth, window.innerHeight);
+  // effectFXAA.setSize(window.innerWidth, window.innerHeight);
+  // customOutline.setSize(window.innerWidth, window.innerHeight);
+  // effectFXAA.uniforms["resolution"].value.set(
+  //   1 / window.innerWidth,
+  //   1 / window.innerHeight
+  // );
 }
 window.addEventListener("resize", onWindowResize, false);
 
 //
 // SCROLL EVENT LISTENER
 //
-
-const wrapper = document.getElementById('wrapper');
 let scrollY = window.scrollY;
 
 window.addEventListener('scroll', () =>
@@ -323,7 +324,6 @@ window.addEventListener('scroll', () =>
 
 
 // ANIMATION
-const clock = new THREE.Clock();
 let step = 0;
 const speed = 0.004;
 
@@ -333,10 +333,10 @@ function animate() {
   const smileModel = scene.getObjectByName('smile');
   if (smileModel) {
     step += speed;
-    smileModel.rotation.y = Math.sin(step * 15) * 0.2 + 1.4;
+    smileModel.rotation.y = Math.sin(step * 15) * 0.2;
   }
 
-  // parallax movement and totations
+  // parallax movement and rotations
   const liquidModel = scene.getObjectByName('liquid');
   if (liquidModel) {
     liquidModel.position.y = - scrollY / window.innerHeight * objectsDistance * 0.95 - 17;
@@ -344,20 +344,21 @@ function animate() {
     liquidModel.rotation.x = 0.00005 * scrollY;
     liquidModel.rotation.y = - 0.00005 * scrollY - 0.1;
   }
-  const flowerModel = outlineScene.getObjectByName('flower');
-  if (flowerModel) {
-    flowerModel.position.x = 0.0001 * scrollY - 10;
-    flowerModel.position.y = - scrollY / window.innerHeight * objectsDistance * 1.004 - 28;
-    flowerModel.position.z = - 0.0005 * scrollY;
-    flowerModel.rotation.x = 0.00005 * scrollY + 0.3;
-    flowerModel.rotation.y = - 0.00005 * scrollY - 0.1 + 270;
-  }
+  // const flowerModel = outlineScene.getObjectByName('flower');
+  // if (flowerModel) {
+  //   flowerModel.position.x = 0.0001 * scrollY - 10;
+  //   flowerModel.position.y = - scrollY / window.innerHeight * objectsDistance * 1.004 - 28;
+  //   flowerModel.position.z = - 0.0005 * scrollY;
+  //   flowerModel.rotation.x = 0.00005 * scrollY + 0.3;
+  //   flowerModel.rotation.y = - 0.00005 * scrollY - 0.1 + 270;
+  // }
 
   // Animate camera
   outlineCamera.position.y = -scrollY / window.innerHeight * objectsDistance - 7.3;
 
-  renderer.render(scene, camera);
-  outlineComposer.render(outlineScene, outlineCamera);
+  // renderer.render(scene, camera);
+  renderer.render(outlineScene, outlineCamera);
+  // outlineComposer.render(outlineScene, outlineCamera);
 }
 
 animate()
